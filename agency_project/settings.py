7dd466147b615182638abbebb9264b5cdedaa848
Martin Vernazza <martinver163@gmail.com>
2025-11-10 17:05:06 -0300

    correcto funcionamiento

--- agency_project/settings.py
+++ agency_project/settings.py
@@ -80,14 +80,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# Email settings
+# Email settings - Gmail (funciona 100%)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_USER = os.environ.get('GMAIL_USER', 'martinver163@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_APP_PASSWORD', 'hizt btma rtya jpws')
+DEFAULT_FROM_EMAIL = os.environ.get('GMAIL_USER', 'martinver163@gmail.com')
 
 # Login settings
 LOGIN_URL = '/login/'
